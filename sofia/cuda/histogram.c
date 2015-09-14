3aed6394a3075079d906f814cacd10f2afdee1a3
Jarred de Beer <jarr2000@gmail.com>
2015-09-14 14:29:15 +0200

    adding histogram files for getRMS

---
+++ sofia/cuda/histogram.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <math.h>
+
+void histogram(float* ary, double* bins, int* histo, int ary_len, int bin_len, int histo_len) {
+
+    double min = bins[0];
+    double bin_size = bins[1] - bins[0];
+
+    for (int i = 0; i < ary_len; i++) {
+        int bin = (ary[i] - min) / bin_size;
+        if (bin >= 0 && bin < histo_len) {
+            histo[bin]++;
+        }
+    }
+
+
+}
---
+++ sofia/cuda/histogram.h
@@ -0,0 +1,1 @@
+void histogram(float* ary, double* bins, int* histo, int ary_len, int bin_len, int histo_len);
